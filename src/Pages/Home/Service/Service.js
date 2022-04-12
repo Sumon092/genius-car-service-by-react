b019a2a8fefcedd903ccbc6df70f5a0fd1bce1e5
sumon092 <m.sumon@outlook.com>
2022-04-13 00:05:29 +0600

    responsive done

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({ service }) => {
     const { name, price, description, img } = service;
     return (
-        <div>
+        <div className='service-detail'>
             <img src={img} alt="" />
             <h2>{name}</h2>
+            <p>$ <small>{price}</small></p>
+            <p>{description}</p>
+            <button>Book {name}</button>
         </div>
     );
 };
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from './Service/Service';
+import './Services/Services.css'
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -11,15 +12,17 @@ const Services = () => {
     }, [])
     return (
         <div>
-            <h2>This is services :{services.length}</h2>
+            <h1 className='service-title'>Our services</h1>
 
-            {
-                services.map(service =>
-                    <Service
-                        key={service.id}
-                        service={service}>
-                    </Service>)
-            }
+            <div className="service-container">
+                {
+                    services.map(service =>
+                        <Service
+                            key={service.id}
+                            service={service}>
+                        </Service>)
+                }
+            </div>
         </div>
 
     );
